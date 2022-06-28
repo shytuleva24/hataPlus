76893d6c285cb75c36587baf13f6e0fedcd94dd4
Ольга Шутылева <99029912+shytuleva24@users.noreply.github.com>
2022-06-28 20:12:46 +0300

    picture in mobile adapt

--- js/main.js
+++ js/main.js
@@ -1,7 +1,16 @@
-console.log($)
-$(function(){
-    const $menu = $('#menu')
-    $('#iconMenu').on('click', function(event){
-        $menu.toggle()
-    }) 
-}) 
+// console.log($)
+// $(function(){
+//     const $menu = $('#menu')
+//     $('#iconMenu').on('click', function(event){
+//         $menu.toggle()
+//     }) 
+// }) 
+
+let hamburger = document.querySelector(".hamburger");
+let menu = document.querySelector(".menu");
+
+hamburger.onclick = function () {
+    menu.classList.toggle ("active-burger");
+    hamburger.classList.toggle ("hamburger-open");
+}
+
